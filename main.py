fd3e2e2c871aa72b9fec6188a37069da2d2e8ffc
RusinovaLena <63599812+RusinovaLena@users.noreply.github.com>
2020-06-04 17:01:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import methods as m
+import pandas
+from sklearn.preprocessing import OneHotEncoder
+import numpy
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.compose import ColumnTransformer
+file_train = pandas.read_csv("train2.csv", header=None)
+file_test = pandas.read_csv("test2.csv", header=None)
+file_output = "lab3.csv"
+data_x = numpy.asarray(file_train.drop(14, axis = 1))
+data_y = numpy.asarray(file_train[14])
+tmp = [1, 3, 5, 6, 7, 8, 9, 13]
+simple_imputer = SimpleImputer(missing_values='?', strategy='most_frequent')
+data_x = simple_imputer.fit_transform(data_x)
+file_test = simple_imputer.fit_transform(file_test)
+onehotencoder = OneHotEncoder(handle_unknown='ignore')
+ctransformer = ColumnTransformer([("onehot", onehotencoder, tmp)], remainder='passthrough')
+data_x = ctransformer.fit_transform(data_x)
+file_test = ctransformer.transform(file_test)
+data_y = numpy.array(data_y, dtype=int)
+c_v = StratifiedShuffleSplit(n_splits=5, test_size=0.2)
+grid_search_cv = m.my_method(data_x, data_y, c_v)
+print("The best estimator: ")
+print(grid_search_cv.best_estimator_)
+print("The best score: ")
+print(grid_search_cv.best_score_)
+print("The best system parameters: ")
+print(grid_search_cv.best_params_)
+y = grid_search_cv.predict(file_test)
+y = numpy.array(y, dtype=int)
+csv_output = pandas.DataFrame(y)
+csv_output.to_csv(file_output, header=False, index=False)
+
---
+++ methods.py
@@ -0,0 +1,27 @@
+import xgboost as xgb
+import numpy
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.svm import SVC
+count = 2
+
+
+def my_method(data_x, data_y, c_v):
+    if count == 1:
+        svm_classifier = SVC(max_iter=10000)
+        grid_prm = {'gamma' : ['scale', 'auto'],
+                    'kernel': ['linear', 'poly', 'rbf', 'sigmoid']}
+        grid_search_cv = GridSearchCV(svm_classifier, grid_prm, scoring = "accuracy", cv = c_v)
+        grid_search_cv.fit(data_x, data_y)
+    else:
+        svm_classifier = xgb.XGBClassifier(max_iter=10000)
+        grid_prm = {'booster' : ['gbtree', 'gblinear', 'dart'],
+                    'eta' : numpy.linspace(0.1, 0.5, num=5),
+                    'sampling_method' : ['uniform', 'gradient_based'],
+                    'tree_method'  : ['approx', 'hist', 'auto', 'exact']}
+        grid_search_cv = RandomizedSearchCV(svm_classifier, grid_prm, scoring = "accuracy", cv = c_v, n_iter = 20, random_state = 0)
+        grid_search_cv.fit(data_x, data_y)
+    return grid_search_cv
+
+
