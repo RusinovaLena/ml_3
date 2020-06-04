4562bb9b4b6f3a3b559ba6643e1aa4888b2b9813
RusinovaLena <63599812+RusinovaLena@users.noreply.github.com>
2020-06-04 19:43:53 +0300

    Update methods.py

--- methods.py
+++ methods.py
@@ -4,18 +4,18 @@ import warnings
 warnings.filterwarnings('ignore')
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.svm import SVC
-count = 2
+v = 2
 
 
 def my_method(data_x, data_y, c_v):
-    if count == 1:
-        svm_classifier = SVC(max_iter=10000)
+    if v == 1:
+        svm_classifier = SVC(max_iter=100000)
         grid_prm = {'gamma' : ['scale', 'auto'],
                     'kernel': ['linear', 'poly', 'rbf', 'sigmoid']}
         grid_search_cv = GridSearchCV(svm_classifier, grid_prm, scoring = "accuracy", cv = c_v)
         grid_search_cv.fit(data_x, data_y)
     else:
-        svm_classifier = xgb.XGBClassifier(max_iter=10000)
+        svm_classifier = xgb.XGBClassifier(max_iter=100000)
         grid_prm = {'booster' : ['gbtree', 'gblinear', 'dart'],
                     'eta' : numpy.linspace(0.1, 0.5, num=5),
                     'sampling_method' : ['uniform', 'gradient_based'],
